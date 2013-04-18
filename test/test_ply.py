609b9576ce505acb91a03389a96586b9f98e71a3
Christophe Pradal <christophe.pradal@inria.fr>
2013-04-18 17:08:40 +0200

    ENH - Read VTK files as a set of PlantGL shapes

--- test/test_ply.py
+++ test/test_ply.py
@@ -25,31 +25,4 @@ print 'Case VTK ', t2-t1
 t1=t2
 
 
-fn = '/media/pradal/DONNEES/pradal/data/plantscan/segmented/segmentedMesh.vtk'
-import numpy as np
-from mayavi.sources.vtk_file_reader import VTKFileReader
 
-"""
-r = VTKFileReader()
-r.initialize(fn)
-mesh = r.outputs[0] 
-points = mesh.points.to_array()
-x, y, z = points.T
-points = np.array((x,y,z)).T
-polys = mesh.polys
-faces = polys.to_array()
-
-faces = faces.reshape((polys.number_of_cells,polys.max_cell_size+1))
-ids, index_1, index_2, index_3 = faces.T
-
-scalars = mesh.point_data.scalars.to_array()
-set_scalars = set(scalars)
-
-idxs = np.indices((len(points),))[0]
-s = 0
-mask = idxs[scalars==s]
-
-for s in set_scalars:
-    mask = idxs[scalars==s]
-    
-"""
